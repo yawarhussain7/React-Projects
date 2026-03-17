5642627982eef2e5b1973501c082e4b7ab860360
Yawar Hussain <yawarhussain793@gmail.com>
2026-03-18 02:08:39 +0500

    AUth UI done

---
+++ EMS/src/App.jsx
@@ -0,0 +1,10 @@
+import Login from "./Components/Auth/Login"
+const App = () => {
+  return (
+    <>
+    <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ EMS/src/Components/Auth/Login.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+const Login = () => {
+    const [formdata,setformdata]=useState({
+        email:"",
+        password:""
+    })
+    // twoway bounding 
+    const submitHandler=(e)=>{
+        e.preventDefault()
+        console.log(formdata)
+        
+       setformdata({
+        email:"",
+        password:""
+       })
+        
+    }
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+            
+        <div className="border-2 border-emerald-600 p-20 rounded-xl">
+            <form className="flex flex-col items-center justify-center  " onSubmit={submitHandler}>
+                <input value={formdata.email} onChange={(e)=>{setformdata({...formdata,email:e.target.value})}} required className=" outline-none bg-transparent  border-2 border-emerald-600 rounded-full px-3 py-5 text-xl text-white placeholder:text-gray-400" type="email" placeholder="Enter your email" />
+                <input value={formdata.password} onChange={(e)=>{setformdata({...formdata,password:e.target.value})}} required className=" outline-none bg-transparent  border-2 mt-3 border-emerald-600 rounded-full px-3 py-5 text-xl text-white placeholder:text-gray-400" type="password" placeholder="Enter your password " />
+                <button className=" mt-5 outline-none  border-none bg-emerald-600 rounded px-3 py-5 text-xl text-white hover:bg-emerald-800" type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
